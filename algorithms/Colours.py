f4e92d09fd64ee350e8a7276507c855f52415643
Max Tyler <mtyler88@gmail.com>
2016-12-21 14:05:11 +0000

    Colour palettes work

--- algorithms/Colours.py
+++ algorithms/Colours.py
@@ -1,14 +1,21 @@
 import requests
 import json
 
+def from_colour_lovers(palette):
+    return ['#'+i for i in palette]
+
 def get_palettes(num_palettes=20, page=0):
     request=requests.get('http://www.colourlovers.com/api/palettes/top', params={'format':'json', 'numResults':str(num_palettes), 'resultOffset':str(page)})
     loaded=json.loads(request.text)
-    return [[palette['title'], palette['colors']] for palette in loaded]
+    return [[palette['title'], from_colour_lovers(palette['colors'])] for palette in loaded]
 
 def get_static_palettes(num_palettes=20):
-    with open('static_palettes.txt', 'r') as read_file:
-        static_palettes = json.load(read_file)
+    with open('algorithms/static_palettes.txt', 'r') as read_file:
+        prev_static_palettes = json.load(read_file)
+    static_palettes=[]
+    for i in prev_static_palettes:
+        static_palettes.append([i[0], from_colour_lovers(i[1])])
+
     if num_palettes>=100: return static_palettes
     else: return static_palettes[:num_palettes]
 
--- algorithms/Helpers.py
+++ algorithms/Helpers.py
@@ -3,6 +3,21 @@ from random import *
 def hex_to_RGB(hex_value):
     return ((hex_value>>8>>8)%256, (hex_value>>8)%256, hex_value%256)
 
+def text_hex_to_RGB(text_hex):
+    int_value=int(text_hex[1:], 16)
+    return hex_to_RGB(int_value)
+
+def RGB_to_hex(rgb):
+    return hex((rgb[0]<<16)+(rgb[1]<<8)+rgb[2])
+
+def RGB_to_text_hex(rgb):
+    return ('#'+RGB_to_hex(rgb)[2:])
+
+def push_hex_values(text_hex, shift):
+    rgb=text_hex_to_RGB(text_hex)
+    new_rgb=[(i+shift)%256 for i in rgb]
+    return RGB_to_text_hex(new_rgb)
+
 class Point:
     def __init__(self, x, y):
         self.x=x
